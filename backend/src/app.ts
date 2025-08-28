4a0355780e2174421573df8c043c7c9a8600fd42
kub938 <kub938@naver.com>
2025-08-29 01:37:04 +0900

    feat: db연결 완료

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import userRouter from "./modules/user/user.routes";
+import { connect_db } from "./config/db";
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use("/api", userRouter);
 
 app.get("/", (req, res) => {
   res.json({
@@ -12,6 +15,13 @@ app.get("/", (req, res) => {
   });
 });
 
-app.listen(PORT, () => {
-  console.log(`${PORT}번 포트 에서 서버 실행중 입니다!`);
-});
+connect_db()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`${PORT}번 포트 에서 서버 실행중 입니다!`);
+    });
+  })
+  .catch((error) => {
+    console.error(" 데이터 베이스 연결 실패", error);
+    process.exit(1);
+  });
