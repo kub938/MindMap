d47fd77612da6a2c479f102f081ab99401f5dd38
김윤배 <kub938@naver.com>
2026-02-06 21:43:29 +0900

    feat: api endpoint 앞에 api/v1(api version)을 붙임

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-import { ValidationPipe } from "@nestjs/common";
+import { ValidationPipe, VersioningType } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +11,13 @@ async function bootstrap() {
       transform: true, // 데이터를 DTO 타입에 맞게 자동 형변환
     }),
   );
+
+  app.setGlobalPrefix("api");
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: "1",
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
